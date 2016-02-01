9bfaf719ec26e767cb56fa0919c400bad8ae076a
bezarsnba <bezarsnba@gmail.com>
2016-02-01 21:40:13 -0200

    Exercicio Algoritmo

---
+++ exercicio_2.py
@@ -0,0 +1,13 @@
+# -*- coding: LATIN1 -*-
+# Exercicio
+# Faça um algoritmo que calcule a área de um triângulo, considerando a fórmula
+# Area = Base x Altura/2 . Utilize as variáveis AREA, BASE e ALTURA e os operadores aritméticos de multiplicação e divisão.
+
+Base=input("Digite a Base somente numero")
+Altura=input("Digite a Altura somente numero")
+
+
+
+AreaTriangulo= ( Base * Altura  /2 )
+
+print "Area =  {0}m²".format(AreaTriangulo)
