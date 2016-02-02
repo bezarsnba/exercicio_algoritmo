d5785f4520efeef76bd99d5e89c632412d80a8a3
bezarsnba <bezarsnba@gmail.com>
2016-02-02 20:04:39 -0200

    Exercicio 3

---
+++ exercicio_3.py
@@ -0,0 +1,15 @@
+# -*- coding: LATIN1 -*-
+#Exercicio 3 Faça um algoritmo que:
+#a) Leia um número inteiro;
+#b) Leia um segundo número inteiro;
+#c) Efetue a adição dos dois valores;
+#d) Apresente o valor calculado.
+
+
+NumInt1=input("Digite um numero inteiro")
+NumInt2=input("Digite outro numero inteiro")
+
+Adicao = NumInt1 + NumInt2
+
+print "Resultado: {0}".format(Adicao)
+
